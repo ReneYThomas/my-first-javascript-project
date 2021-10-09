e520dc10f5ca6ca335f47eff1c32be43d69347a0
Rene Thomas <renethomas@Rene-Thomas.local>
2021-10-09 16:49:55 -0500

    JS-01: (arithmetic operation activity) calculated sum of 2 integers, displayed in terminal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 console.log('Hello Project');
 
+// Activty 1: Calculate the sum of 2 numbers and display it in the terminal
+let num1 = 3
+let num2 = 2
+let num3 = 4
+let sum = num2 + num1 
+console.log('sum is', sum);
 
